4b308ac1644ea0ebd09ff4b2aa5fe8127df76217
kennacapri <kennac28@gmail.com>
2022-10-18 12:11:34 -0600

    added color coding for time blocks to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
     $('#currentDay').text(moment().format("MMMM Do, YYYY"));
+
+  function hourTracker() {
+    var currentHour =  moment().hour();
+
+    $(".time-block").each(function() {
+      var hourlyBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+      // change color coding to match past, present and future
+    if (hourlyBlock < currentHour) {
+      $(this).addClass("past");
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+    }
+     else if (hourlyBlock === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).removeClass("future");
+     }      
+     else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future"); 
+     }
   })
+} hourTracker();
+})
